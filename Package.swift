c27c89b08731370fc6eeaf172ad9222116416d60
Maxim Vasin <maxim.vasin.2@gmail.com>
2019-07-02 11:24:43 +0300

    New Package with tests

--- Package.swift
+++ Package.swift
@@ -13,13 +13,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "0.1.0"),
+        // .package(url: /* package url */, from: "0.4.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "AIToolbox",
-            dependencies: [])
+            dependencies: []),
+        .testTarget(
+            name: "AIToolboxTests",
+            dependencies: ["AIToolbox"]),
     ]
 )
