df40d0e2757afc9ec404a5f589ab2f21bcd9aeb5
Maxim Vasin <maxim.vasin.2@gmail.com>
2019-07-02 11:49:27 +0300

    path to targets in Package.swift

--- Package.swift
+++ Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "AIToolbox",
+	    path: ["AIToolbox", "Package"],	
             dependencies: []),
         .testTarget(
             name: "AIToolboxTests",
+            path: ["AIToolboxTests"],  		
             dependencies: ["AIToolbox"]),
     ]
 )
